5fae4b8e5dbcb0393a6aa304f6380407326f4a04
Ambareen09 <ambubushra@gmail.com>
2023-06-16 00:17:22 +0530

    displaying typed messages in page

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Password@localhost/flask_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
