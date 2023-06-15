fb5a239c1e9ae09ae2507946d001df70c2bf93ec
Ambareen09 <ambubushra@gmail.com>
2023-06-15 20:58:44 +0530

    idk

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Password@localhost/flask_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 socketio = SocketIO(app)
+db = SQLAlchemy(app)
+
 
 from app import views
 from app import admin_views
