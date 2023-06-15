1f332b2938522a34ebe13e8dc2af1355974be1f4
Ambareen09 <ambubushra@gmail.com>
2023-06-15 23:55:49 +0530

    display user db contents

--- app/app/views.py
+++ app/app/views.py
@@ -1,10 +1,21 @@
-from app import app, socketio
-from flask import render_template, Flask, jsonify
+from app import app, socketio, db
+from flask import render_template, jsonify
+from app.models import User
 import os
+
 
 @app.route("/")
 def index():
-    return render_template("public/index.html")
+    users = User.query.all()
+    if len(users) == 0:
+        # Initialize a row with default values
+        default_user = User(name='Delilah Doe', email='delilah@example.com')
+        db.session.add(default_user)
+        db.session.commit()
+        users = [default_user]
+
+    return render_template("public/index.html", users=users)
+
 
 @app.route("/about")
 def about():
@@ -18,6 +29,22 @@ def get_env_variable():
         return jsonify({'error': f'Environment variable {env_var_name} not found, use -> export MY_ENV_VARIABLE=your_value to set it'}), 404
     else:
         return jsonify({'env_variable': env_var_value})
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    if len(users) == 0:
+        # Initialize a row with default values
+        default_user = User(name='Ambareen Azam', email='ambu@example.com')
+        db.session.add(default_user)
+        db.session.commit()
+        users = [default_user]
+
+    user_data = []
+    for user in users:
+        user_data.append({'id': user.id, 'name': user.name, 'email': user.email})
+
+    return jsonify({'users': user_data})
 
 @socketio.on('message')
 def handle_message(message):
